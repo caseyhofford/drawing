3f036b069ea23652f83d0304738db779c7073ae2
Casey Hofford <casey.hofford@gmail.com>
2015-05-08 14:23:13 -0600

    beginning the html and color picker

--- paint_client.js
+++ paint_client.js
@@ -1,5 +1,8 @@
 var size = 1000;
 
+var pen_color = "#00000";
+var pen_size = 1;
+
 function load()
 {
   var table = getElementById("table");
@@ -11,7 +14,23 @@ function load()
       var cell = row.appendChild(td);
       cell.x = i;
       cell.y = j;
-      cell.addEventListener()
+      cell.addEventListener(onmousedown, sendColor(i,j));
+      cell.addEventListener(onmousemove, sendColor(i,j));
     }
   }
+}
+
+function sendColor(x,y)
+{
+  var url = "change_cells?x="+x+"&y="+y+"&color="+pen_color;
+  var send_change = new XMLHttpRequest;
+  send.change.open("get", url);
+  send.change.send();
+}
+
+function changePenColor()
+{
+  var color = this.id;
+  //here we could either use hex color codes to specify color or keep the color name and specify the css color just by name.
+  pen_color = color;
 }
