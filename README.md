eccee7935d2fd670dc55bff2f55da5313ca20d31
SWalls <soerenwalls@hotmail.com>
2015-05-08 12:43:49 -0600

    Updated some code for the server file. Isn't complete yet.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# drawing
+# App Design Final Project: Multi-User Drawing
+By Casey Hofford and Soeren Walls
+
+This node.js app allows multiple people to draw on the same canvas in their browser simultaneously.
--- paint_server.js
+++ paint_server.js
@@ -1,72 +1,144 @@
 var fs = require("fs");
 var http = require("http");
-var recently_changed = [];
 
-var xcolors = [];
-for(var i = 0; i < 1000; i++)
+var CANVAS_SIZE = 1000;
+
+/* Array of objects {x,y,c} representing cells that have been changed. */
+var recentlyChangedCells = [];
+
+/* Multidimensional Array storing all columns and rows of colors in the table. */
+var canvas = new Array();
+for(var i=0; i<CANVAS_SIZE; i++)
 {
-  var ycolors = [];
-  for(var j = 0; j < 1000; j++)
-  {
-    ycolors.push("#FFFFFF");
-  }
+    canvas[i] = new Array();
+    for(var j=0; j<CANVAS_SIZE; j++) {
+        canvas[i][j] = "#ffffff";
+    }
 }
 
-sendCanvas(x,y)//I think we should really send the function x values with ranges of y's as pen strokes and change those ranges all at once, not exactly sure how we'd implement this though.... could be x, y starting point with a size value that always means a certain sized pen with x,y as a vertex (square pen).
-{
-  res.writeHead(200);
-  var changed_cell = xcolors[x][y];
-  res.end(JSON.stringify(changed_cell));f
-}
+init();
 
-addPaint(req, res)
+/* Initialize server on specified port. */
+function init()
 {
-  var input = req.substring(11).split("&");
-  var x = input[0].split("=")[1];//maybe should be a couple lines of code?
-  var y = /input[1].split("=")[1];
-  var color = input[2].split("=")[1];
-  //var size = input[3].split("=")[1];
-  xcolors[x][y] = color;//with size idea this would be in two for loops
-  sendCanvas(x,y);
-}
+    var server = http.createServer( serverFn );
 
-serveFile(file,req,res)
-{
-  var contents = "";
-  try {
-    contents = fs.readFileSync( filename ).toString();
-  }
-  catch( e ) {
-    console.log(
-        "Error: Something bad happened trying to open "+filename );
-      res.writeHead( 404 );
-      res.end( "" );
-      return;
-  }
+    var port;
+    if( process.argv.length < 3 )
+    {
+        port = 8080;
+    }
+    else
+    {
+        port = parseInt( process.argv[2] );
+    }
 
-  res.writeHead( 200 );
-  res.end( contents );
+    server.listen( port );
 }
 
 function serverFn(req,res)
 {
-  if(req.substring(1,15) == request_canvas)
-  {
-    sendCanvas(req,res);
-  }
-  else if(req.substring(1,11) == send_paint)
-  {
-    addPaint(req, res);
-  }
-  else if(filename == 'paint_client.js')
-  {
-    serveFile('paint_client.js'), res, res);
-  }
-  else
-  {
-    serveFile("paint.html", req, res);
-  }
+    var filename = req.url.substring( 1, req.url.length );
+    
+    if(filename === "")
+    {
+        filename = "./index.html";
+    }
+    
+    if(filename.indexOf("get_changed_cells") > -1)
+    {
+        sendChangedCells(req, res);
+    }
+    else if(filename.indexOf("change_cells") > -1)
+    {
+        // We are changing some cells (maybe).
+        filename = "./index.html";
+        var urlData = filename.split("change_cells")[1];
+        if(urlData.indexOf("?") > -1) {
+            // Parse URL into an array of cells.
+            var cells = getCellsFromUrl( urlData );
+            // Add the specified cells to the cells that need to be updated.
+            changeCells( filename, req, res, cells );
+        } else {
+            // No cells were passed as parameters in the URL, so just return index.html.
+            serveFile( filename, req, res );
+        }
+    }
+    else
+    {
+        serveFile(filename, req, res);
+    }
 }
 
-var server = http.createServer(serverFn);
-server.listen(8080);
+/* Parse URL into an array of cells. */
+function getCellsFromUrl( urlData )
+{
+    var queryData = urlData.split("?")[1];
+    var fields = queryData.split("&");
+    var changedCells = [];
+    for(var i=0; i<fields.length; i++) {
+        var fieldSplit = fields[i].split("=");
+        var fieldValue = fieldSplit[1];
+        var cellCoords = fieldValue.split("%2C"); // split on comma. %2C = ,
+        if(cellCoords.length === 3)
+            changedCells.push({ x: cellCoords[0], y: cellCoords[1], c: cellCoords[2] });
+    }
+    return changedCells;
+}
+
+/* Load a file */
+function serveFile( filename, req, res )
+{
+    try
+    {
+    	var contents = getFileContents( filename );
+    }
+    catch( e )
+    {
+        /* Return a 404 page */
+        fourZeroFour(filename, res);
+        return;
+    }
+    
+    var extension = "html";
+    try {
+        extension = filename.split(/.\./)[1];
+    } catch(e) {
+        // Do nothing.
+    }
+
+    res.writeHead( 200, {'Content-Type':'text/'+extension} );
+    res.end( contents );
+}
+
+/* I think we should really send the function x values 
+ * with ranges of y's as pen strokes and change those ranges
+ * all at once, not exactly sure how we'd implement this though....
+ * could be x, y starting point with a size value that always means
+ * a certain sized pen with x,y as a vertex (square pen). */
+function sendChangedCells(req, res)
+{
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify(recentlyChangedCells));
+    // Empty the array.
+    recentlyChangedCells = [];
+}
+
+function changeCells(filename, req, res, cells)
+{
+    recentlyChangedCells = cells;
+    for(var i=0; i<recentlyChangedCells.length; i++)
+    {
+        canvas[recentlyChangedCells[i].x][recentlyChangedCells[i].y]
+            = recentlyChangedCells[i].c;
+    }
+    serveFile(filename, req, res);
+}
+
+/* Return a 404 page */
+function fourZeroFour(filename, res) {
+	console.log( "Error: Something bad happened trying to open "+filename );
+    res.writeHead(404, {'Content-Type':'text/plain'});
+    res.write("404 : File Not Found");
+    res.end();
+}
